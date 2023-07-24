72b016ee2fb5f4a584fe563b75550d59969a9c0d
Iñigo Urteaga <urteaga@gmail.com>
2023-07-24 17:51:39 +0200

    Tested filtering and SGD code, it runs!

--- src/cdlgssm_test.py
+++ src/cdlgssm_test.py
@@ -21,26 +21,31 @@ key1, key2 = jr.split(jr.PRNGKey(0))
 
 # Model def
 inputs=None # Not interested in inputs for now
-model=LinearGaussianSSM(state_dim=2, emission_dim=5)
-params, param_props = model.initialize(key1)
+d_model=LinearGaussianSSM(state_dim=2, emission_dim=5)
+d_params, d_param_props = d_model.initialize(key1)
 
 # Discrete sampling
 NUM_TIMESTEPS = 100
 
 # Simulate from discrete model
 print('Simulating in discrete time')
-d_states, d_emissions = model.sample(
-    params,
+d_states, d_emissions = d_model.sample(
+    d_params,
     key2,
     num_timesteps=NUM_TIMESTEPS,
     inputs=inputs
 )
 
 pdb.set_trace()
+print('Discrete time filtering')
+from dynamax.linear_gaussian_ssm.inference import lgssm_filter
+d_filtered_posterior = lgssm_filter(d_params, d_emissions, inputs)
+pdb.set_trace()
+
 print('Fitting discrete time with SGD')
-d_sgd_fitted_params, d_sgd_lps = model.fit_sgd(
-    params,
-    param_props,
+d_sgd_fitted_params, d_sgd_lps = d_model.fit_sgd(
+    d_params,
+    d_param_props,
     d_emissions,
     inputs=inputs,
     num_epochs=10
@@ -56,13 +61,13 @@ key1, key2 = jr.split(jr.PRNGKey(0))
 
 # Model def
 inputs=None # Not interested in inputs for now
-cdmodel=ContDiscreteLinearGaussianSSM(state_dim=2, emission_dim=5)
-cdparams, cdparam_props = cdmodel.initialize(key1)
+cd_model=ContDiscreteLinearGaussianSSM(state_dim=2, emission_dim=5)
+cd_params, cd_param_props = cd_model.initialize(key1)
 
 # Simulate from continuous model
 print('Simulating in continuous-discrete time')
-cd_states, cd_emissions = cdmodel.sample(
-    cdparams,
+cd_states, cd_emissions = cd_model.sample(
+    cd_params,
     key2,
     num_timesteps=NUM_TIMESTEPS,
     t_emissions=t_emissions,
@@ -73,11 +78,15 @@ pdb.set_trace()
 assert jnp.allclose(d_states, cd_states)
 assert jnp.allclose(d_emissions, cd_emissions)
 
+print('Continuous-Discrete time filtering')
+from continuous_discrete_linear_gaussian_ssm.inference import cdlgssm_filter, cdlgssm_smoother, cdlgssm_posterior_sample
+cd_filtered_posterior = cdlgssm_filter(cd_params, cd_emissions, t_emissions, inputs)
+
 pdb.set_trace()
 print('Fitting continuous-discrete time with SGD')
-cd_sgd_fitted_params, cd_sgd_lps = cdmodel.fit_sgd(
-    cdparams,
-    cdparam_props,
+cd_sgd_fitted_params, cd_sgd_lps = cd_model.fit_sgd(
+    cd_params,
+    cd_param_props,
     cd_emissions,
     t_emissions,
     inputs=inputs,
@@ -99,28 +108,28 @@ assert jnp.allclose(d_sgd_fitted_params.dynamics.bias,
                      cd_sgd_fitted_params.dynamics.bias)
 
 print('Checking that emission weights are close...')
-assert jnp.allclose(d_sgd_fitted_params.emission.weights,
-                    cd_sgd_fitted_params.emission.weights)
+assert jnp.allclose(d_sgd_fitted_params.emissions.weights,
+                    cd_sgd_fitted_params.emissions.weights)
 
 print('Checking that emission biases are close...')
-assert jnp.allclose(d_sgd_fitted_params.emission.bias,
-                        cd_sgd_fitted_params.emission.bias)
+assert jnp.allclose(d_sgd_fitted_params.emissions.bias,
+                        cd_sgd_fitted_params.emissions.bias)
 
 print('Checking that initial mean is close...')
-assert jnp.allclose(d_sgd_fitted_params.initial_mean,
-                    cd_sgd_fitted_params.initial_mean)
+assert jnp.allclose(d_sgd_fitted_params.initial.mean,
+                    cd_sgd_fitted_params.initial.mean)
 
 print('Checking that initial covariance is close...')
-assert jnp.allclose(d_sgd_fitted_params.initial_covariance,
-                    cd_sgd_fitted_params.initial_covariance)
+assert jnp.allclose(d_sgd_fitted_params.initial.cov,
+                    cd_sgd_fitted_params.initial.cov)
 
 print('Checking that dynamics covariance is close...')
-assert jnp.allclose(d_sgd_fitted_params.dynamics_covariance,
-                    cd_sgd_fitted_params.dynamics_covariance)
+assert jnp.allclose(d_sgd_fitted_params.dynamics.cov,
+                    cd_sgd_fitted_params.dynamics.diff_cov)
 
 print('Checking that emission covariance is close...')
-assert jnp.allclose(d_sgd_fitted_params.emission_covariance,
-                    cd_sgd_fitted_params.emission_covariance)
+assert jnp.allclose(d_sgd_fitted_params.emissions.cov,
+                    cd_sgd_fitted_params.emissions.cov)
 
 print('All tests passed!')
 
