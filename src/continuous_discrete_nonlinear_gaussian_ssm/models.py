398e0e8ce00e99c340768493d303e4f008c8a75a
Iñigo Urteaga <urteaga@gmail.com>
2024-02-23 11:54:35 +0100

    Working fit-sgd after code refactoring, need to test fitting quality

--- src/continuous_discrete_nonlinear_gaussian_ssm/models.py
+++ src/continuous_discrete_nonlinear_gaussian_ssm/models.py
@@ -12,35 +12,27 @@ import jax.debug as jdb
 
 from cdssm_utils import diffeqsolve
 
-from typing import NamedTuple, Tuple, Optional, Union, Callable
+from typing import NamedTuple, Tuple, Optional, Union, List
 import tensorflow_probability.substrates.jax as tfp
 from tensorflow_probability.substrates.jax.distributions import MultivariateNormalFullCovariance as MVN
 import tensorflow_probability.substrates.jax.distributions as tfd
 
+# Dynamax shared code
+from dynamax.types import Scalar
 from dynamax.parameters import ParameterProperties, ParameterSet
 from dynamax.utils.bijectors import RealToPSDBijector
+from dynamax.linear_gaussian_ssm.inference import PosteriorGSSMFiltered, PosteriorGSSMSmoothed
 
 tfd = tfp.distributions
 tfb = tfp.bijectors
 
 # Our codebase
 from ssm_temissions import SSM
-# To avoid unnecessary redefinitions of code,
-# We import parameters and posteriors that can be reused from LGSSM first
-# And define the rest later
-from dynamax.linear_gaussian_ssm.inference import ParamsLGSSMInitial
-
-### For learnable functions
-import abc
-
-FnStateToState = Callable[ [Float[Array, "state_dim"]], Float[Array, "state_dim"]]
-FnStateToStateByState = Callable[ [Float[Array, "state_dim"]], Float[Array, "state_dim state_dim"]]
-FnStateToStateByStateByState = Callable[ [Float[Array, "state_dim"]], Float[Array, "state_dim state_dim state_dim"]]
-FnStateAndInputToState = Callable[ [Float[Array, "state_dim"], Float[Array, "input_dim"]], Float[Array, "state_dim"]]
-FnStateAndInputToStateByState = Callable[ [Float[Array, "state_dim"], Float[Array, "input_dim"]], Float[Array, "state_dim state_dim"]]
-FnStateAndInputToStateByStateByState = Callable[ [Float[Array, "state_dim"], Float[Array, "input_dim"]], Float[Array, "state_dim state_dim state_dim"]]
-FnStateToEmission = Callable[ [Float[Array, "state_dim"]], Float[Array, "emission_dim"]]
-FnStateAndInputToEmission = Callable[ [Float[Array, "state_dim"], Float[Array, "input_dim"] ], Float[Array, "emission_dim"]]
+# CDNLGSSM param and function definition
+from continuous_discrete_nonlinear_gaussian_ssm.cdnlgssm_utils import *
+from continuous_discrete_nonlinear_gaussian_ssm.inference_ekf import EKFHyperParams, iterated_extended_kalman_filter
+from continuous_discrete_nonlinear_gaussian_ssm.inference_enkf import EnKFHyperParams, ensemble_kalman_filter
+from continuous_discrete_nonlinear_gaussian_ssm.inference_ukf import UKFHyperParams, unscented_kalman_filter
 
 # TODO: This function is defined in many places... unclear whether we need to redefine, or move to utils   
 def _get_params(x, dim, t):
@@ -53,190 +45,6 @@ def _get_params(x, dim, t):
         return x[t]
     else:
         return x
-
-# TODO: do we need @dataclass(frozen=True)?
-class LearnableFunction(abc.ABC):
-    ''' All Learnable functions should have
-        params propertie
-        a definiton of a function that takes as input x, u and t
-    '''
-    # Parameters as properties of the class
-    params: ParameterSet
-    
-    def __init__(
-        self,
-        params,
-    ):
-        self.params = params
-    
-    # A function definition   
-    @abc.abstractmethod
-    def f(self, x, u=None, t=None):
-        ''' A function to be defined by specific classes
-            With inputs
-            x: state
-            u: inputs
-            t: time
-        '''
-
-# Learnable Constan Function class
-# TODO: figure out how to deal with different dimensionalities
-class ConstantLearnableFunction(LearnableFunction):
-    # Linear weights for the state
-    # TODO: figure out how to have more than
-    params: Union[Float[Array, "state_dim state_dim"], ParameterProperties]
-
-    def f(self, x, u=None, t=None):
-        return self.params
-
-# Learnable Linear Function class
-class LinearLearnableFunction(LearnableFunction):
-    # Linear weights for the state
-    # TODO: figure out how to have more than
-    params: Union[Float[Array, "state_dim state_dim"], ParameterProperties]
-    
-    def f(self, x, u=None, t=None):
-        return self.params @ x
-
-# Continuous non-linear Gaussian dynamics
-# TODO: function definitions within parameter classes breaks fit_sgd: where should they be placed?
-class ParamsCDNLGSSMDynamics(NamedTuple):
-    r"""Parameters of the state dynamics of a CDNLGSSM model.
-
-    This model does not obey an SDE as in Sarkaa's equation (3.151):
-        the solution to 3.151 is not necessarily a Gaussian Process
-            (note there are cases where that is indeed the case)
-
-    We instead assume an approximation to the model of zero-th, first or second order
-
-    The resulting transition and emission distributions are
-    $$p(z_1) = N(z_1 | m, S)$$
-    $$p(z_t | z_{t-1}, u_t) = N(z_t | z_t, P_t)$$
-    $$p(y_t | z_t) = N(y_t | h(z_t, u_t), R_t)$$
-
-    If you have no inputs, the dynamics and emission functions do not to take $u_t$ as an argument.
-
-    The tuple doubles as a container for the ParameterProperties.
-
-    :param drift_function: $f$
-    :param drift_parameters: parameters $\theta$ of the drift_function
-    :param diffusion_coefficient: $L$
-    :param diffusion_cov: $Q$
-    :param dynamics_approx: 'zeroth', 'first' or 'second'
-
-    """
-    '''
-    # the deterministic drift $f$ of the nonlinear RHS of the state
-    drift_function: Union[FnStateToState, FnStateAndInputToState]
-    # TODO: How to define learnable parameters for emission function?
-    #drift_parameters: Union[Float[Array], ParameterProperties] 
-    # the coefficient matrix L of the state's diffusion process
-    diffusion_coefficient: Union[Float[Array, "state_dim state_dim"], Float[Array, "ntime state_dim state_dim"], ParameterProperties]
-    # The covariance matrix Q of the state noise process
-    diffusion_cov: Union[Float[Array, "state_dim state_dim"], Float[Array, "ntime state_dim state_dim"], Float[Array, "state_dim_triu"], ParameterProperties]
-    '''
-    
-    # These are all learnable functions to be initialized
-    drift: LearnableFunction
-    diffusion_coefficient: LearnableFunction
-    diffusion_cov: LearnableFunction
-    
-    # Dynamics SDE approximation type
-    approx_type: str
-
-'''
-# Continuous non-linear dynamic parameters
-class ParamsCDNLSSMDynamics(NamedTuple):
-    r"""Parameters of the state dynamics of a CDNLGSSM model.
-
-    This model does obey the SDE as in Sarkaa's equation (3.151):
-        the solution to 3.151 is not necessarily a Gaussian Process
-            (note there are cases where that is indeed the case)
-
-    If you have no inputs, the dynamics and emission functions do not to take $u_t$ as an argument.
-
-    The tuple doubles as a container for the ParameterProperties.
-
-    :param drift_function: $f$
-    :param drift_parameters: parameters $\theta$ of the drift_function
-    :param diffusion_coefficient: $L$
-    :param diffusion_cov: $Q$
-
-    """
-    # the deterministic drift $f$ of the nonlinear RHS of the state
-    drift_function: Union[FnStateToState, FnStateAndInputToState]
-    # TODO: How to define learnable parameters for dynamics drift function?
-    #drift_parameters: Union[Float[Array], ParameterProperties] 
-    # the coefficient matrix L of the state's diffusion process
-    diffusion_coefficient: Union[Float[Array, "state_dim state_dim"], Float[Array, "ntime state_dim state_dim"], ParameterProperties]
-    # The covariance matrix Q of the state noise process
-    diffusion_cov: Union[Float[Array, "state_dim state_dim"], Float[Array, "ntime state_dim state_dim"], Float[Array, "state_dim_triu"], ParameterProperties]
-'''
-   
-# Discrete non-linear emission parameters
-# TODO: function definitions within parameter classes breaks fit_sgd: where should they be placed?
-class ParamsCDNLGSSMEmissions(NamedTuple):
-    r"""Parameters of the state dynamics
-
-    $$p(z_{t+1} \mid z_t, u_t) = \mathcal{N}(z_{t+1} \mid A z_t + B u_t + b, Q)$$
-
-    The tuple doubles as a container for the ParameterProperties.
-
-    :param drift_function: $f$
-    :param drift_parameters: parameters $\theta$ of the drift_function
-    :param diffusion_coefficient: $L$
-    :param diffusion_cov: $Q$
-    :param dynamics_approx: 'zeroth', 'first' or 'second'
-
-    """
-    # These are all learnable functions to be initialized
-    emission_function: LearnableFunction
-    emission_cov: LearnableFunction
-    
-    '''
-    # Emission distribution h
-    emission_function: Union[FnStateToEmission, FnStateAndInputToEmission]
-    # TODO: How to define learnable parameters for emission function?
-    # emission_parameters: Union[Float[Array], ParameterProperties] 
-    # The covariance matrix R of the observation noise process
-    emission_cov: Union[Float[Array, "emission_dim emission_dim"], ParameterProperties]
-    '''
-    
-# CDNLGSSM parameters are different to CDLGSSM due to nonlinearities
-class ParamsCDNLGSSM(NamedTuple):
-    r"""Parameters of a linear Gaussian SSM.
-
-    :param initial: initial distribution parameters
-    :param dynamics: dynamics distribution parameters
-    :param emissions: emission distribution parameters
-
-    The assumed transition and emission distributions are
-    $$p(z_1) = N(z_1 | m, S)$$
-    $$p(z_t | z_{t-1}, u_t) = N(z_t | m_t, P_t)$$
-    $$p(y_t | z_t) = N(y_t | h(z_t, u_t), R_t)$$
-
-    """
-    initial: ParamsLGSSMInitial
-    dynamics: ParamsCDNLGSSMDynamics
-    emissions: ParamsCDNLGSSMEmissions 
-
-'''
-# CDNLSSM parameters are different to CDNLGSSM due to non-gaussian transitions
-class ParamsCDNLGSSM(NamedTuple):
-    r"""Parameters of a linear Gaussian SSM.
-
-    :param initial: initial distribution parameters
-    :param dynamics: dynamics distribution parameters
-    :param emissions: emission distribution parameters
-
-    The assumed transition and emission distributions are
-    $$p(z_1) = N(z_1 | m, S)$$
-    
-    """
-    initial: ParamsLGSSMInitial
-    dynamics: ParamsCDNLSSMDynamics
-    emissions: ParamsCDNLGSSMEmissions 
-'''
    
 # CDNLGSSM push-forward is model-specific
 def compute_pushforward(
@@ -263,14 +71,14 @@ def compute_pushforward(
         #L_t = _get_params(params.dynamics.diffusion_coefficient, 2, t0)
 
         # Different SDE approximations
-        if params.dynamics.approx_type=='zeroth':
+        if params.dynamics.approx_order==0.:
             # Mean evolution
             dxdt = f(x, inputs, t)
             # Covariance evolution
             dPdt = L_t @ Qc_t @ L_t.T
         
         # following Sarkka thesis eq. 3.153
-        elif params.dynamics.approx_type=='first':
+        elif params.dynamics.approx_order==1.:
             # Evaluate the jacobian of the dynamics function at x and inputs
             F_t=jacfwd(f)(x, inputs, t)
         
@@ -280,7 +88,7 @@ def compute_pushforward(
             dPdt = F_t @ P + P @ F_t.T + L_t @ Qc_t @ L_t.T
         
         # follow Sarkka thesis eq. 3.155
-        elif params.dynamics.approx_type=='second':
+        elif params.dynamics.approx_order==2.:
             # Evaluate the jacobian of the dynamics function at x and inputs
             F_t=jacfwd(f)(x, inputs, t)
             # Evaluate the Hessian of the dynamics function at x and inputs
@@ -292,7 +100,7 @@ def compute_pushforward(
             # Covariance evolution
             dPdt = F_t @ P + P @ F_t.T + L_t @ Qc_t @ L_t.T
         else:
-            raise ValueError('params.dynamics.approx_type = {} not implemented yet'.format(params.dynamics.approx_type))
+            raise ValueError('params.dynamics.approx_order = {} not implemented yet'.format(params.dynamics.approx_order))
 
         return (dxdt, dPdt)
     
@@ -358,8 +166,8 @@ class ContDiscreteNonlinearGaussianSSM(SSM):
         dynamics_drift: Optional[LearnableFunction] = None,
         dynamics_diffusion_coefficient: Optional[LearnableFunction] = None,
         dynamics_diffusion_cov: Optional[LearnableFunction] = None,
-        dynamics_approx_type: Optional[str] = 'zeroth',
-        emission_function: Optional[Union[FnStateToEmission, FnStateAndInputToEmission]] = None,
+        dynamics_approx_order: Optional[float] = 2.,
+        emission_function: Optional[LearnableFunction] = None,
         #emission_parameters = None,
         emission_cov: Optional[Float[Array, "emission_dim emission_dim"]] = None
     ) -> Tuple[ParamsCDNLGSSM, PyTree]:
@@ -379,7 +187,7 @@ class ContDiscreteNonlinearGaussianSSM(SSM):
         _dynamics_diffusion_cov = ConstantLearnableFunction(
                                     0.1 * jnp.eye(self.state_dim)
                                 )
-        _dynamics_approx_type = 'second'
+        _dynamics_approx_order = 2.
         # Emission
         _emission_function = LinearLearnableFunction(
                             params=jr.normal(key, (self.emission_dim, self.state_dim))
@@ -402,7 +210,7 @@ class ContDiscreteNonlinearGaussianSSM(SSM):
                 drift=default(dynamics_drift, _dynamics_drift),
                 diffusion_coefficient=default(dynamics_diffusion_coefficient, _dynamics_diffusion_coefficient),
                 diffusion_cov=default(dynamics_diffusion_cov, _dynamics_diffusion_cov),
-                approx_type=default(dynamics_approx_type, _dynamics_approx_type)
+                approx_order=default(dynamics_approx_order, _dynamics_approx_order)
                 ),
             emissions=ParamsCDNLGSSMEmissions(
                 emission_function=default(emission_function, _emission_function),
@@ -427,7 +235,7 @@ class ContDiscreteNonlinearGaussianSSM(SSM):
                 diffusion_cov=ConstantLearnableFunction(
                         params=ParameterProperties(constrainer=RealToPSDBijector())
                     ),
-                approx_type=ParameterProperties(trainable=False)
+                approx_order=ParameterProperties(trainable=False)
                 ),
             emissions=ParamsCDNLGSSMEmissions(
                 emission_function=LinearLearnableFunction(
@@ -479,3 +287,80 @@ class ContDiscreteNonlinearGaussianSSM(SSM):
         mean = params.emissions.emission_function.f(state, inputs, t=None)
         R = params.emissions.emission_cov.f(state, inputs, t=None)
         return MVN(mean, R)
+
+    def marginal_log_prob(
+        self,
+        params: ParamsCDNLGSSM,
+        filter_hyperparams: Optional[Union[EKFHyperParams, EnKFHyperParams, UKFHyperParams]],
+        emissions: Float[Array, "ntime emission_dim"],
+        t_emissions: Optional[Float[Array, "ntime 1"]]=None,
+        inputs: Optional[Float[Array, "ntime input_dim"]] = None,
+    ) -> Scalar:
+        filtered_posterior = self.cdnlgssm_filter(
+            params=params,
+            emissions=emissions,
+            filter_hyperparams=filter_hyperparams,
+            t_emissions=t_emissions,
+            inputs=inputs
+        )
+        return filtered_posterior.marginal_loglik
+        
+    def cdnlgssm_filter(
+        self,
+        params: ParamsCDNLGSSM,
+        emissions: Float[Array, "ntime emission_dim"],
+        filter_hyperparams: Optional[Union[EKFHyperParams, EnKFHyperParams, UKFHyperParams]]=EKFHyperParams(),
+        t_emissions: Optional[Float[Array, "num_timesteps 1"]]=None,
+        inputs: Optional[Float[Array, "ntime input_dim"]] = None,
+        num_iter: Optional[int] = 1,
+        output_fields: Optional[List[str]]=["filtered_means", "filtered_covariances", "predicted_means", "predicted_covariances"],
+    ) -> PosteriorGSSMFiltered:
+        r"""Run an continuous-discrete nonlinear filter to produce the
+            marginal likelihood and filtered state estimates.
+            
+            Depending on the hyperparameter class provided, it can execute EKF, UKF or EnKF
+        
+        Args:
+            params: model parameters.
+            emissions: observation sequence.
+            t_emissions: continuous-time specific time instants of observations: if not None, it is an array 
+            num_iter: number of linearizations around posterior for update step (default 1).
+            hyperparams: hyper-parameters of the filter
+            inputs: optional array of inputs.
+            output_fields: list of fields to return in posterior object.
+                These can take the values "filtered_means", "filtered_covariances",
+                "predicted_means", "predicted_covariances", and "marginal_loglik".
+
+        Returns:
+            post: posterior object.
+
+        """
+        # TODO: this can be condensed, by incorporating num_iter into hyperparams of EKF
+        # TODO: use and leverage output_fields to have more or less granular returned posterior object
+        if isinstance(filter_hyperparams, EKFHyperParams):
+            filtered_posterior=iterated_extended_kalman_filter(
+                params = params,
+                emissions = emissions,
+                t_emissions = t_emissions,
+                hyperparams = filter_hyperparams,
+                inputs = inputs,
+                num_iter = num_iter,
+            )
+        elif isinstance(filter_hyperparams, EnKFHyperParams):
+            filtered_posterior=ensemble_kalman_filter(
+                params = params,
+                emissions = emissions,
+                t_emissions = t_emissions,
+                hyperparams = filter_hyperparams,
+                inputs = inputs,
+            )
+        elif isinstance(filter_hyperparams, UKFHyperParams):
+            filtered_posterior=unscented_kalman_filter(
+                params = params,
+                emissions = emissions,
+                t_emissions = t_emissions,
+                hyperparams = filter_hyperparams,
+                inputs = inputs,
+            )
+        
+        return filtered_posterior
