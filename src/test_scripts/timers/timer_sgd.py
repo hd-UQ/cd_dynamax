24c81fa31f740cb98252769fcdc7f8f25eec6f19
mattlevine22 <mattlevine22@gmail.com>
2024-04-16 12:18:17 -0400

    more timers

---
+++ src/test_scripts/timers/timer_sgd.py
@@ -0,0 +1,119 @@
+import sys
+sys.path.append("../")
+sys.path.append("../..")
+
+import dynamax
+
+from jax import numpy as jnp
+from jax import jit, vmap
+import jax.random as jr
+from matplotlib import pyplot as plt
+
+import argparse
+
+
+# use custom src codebase
+from utils.plotting_utils import *
+
+# from utils.utils import monotonically_increasing
+from continuous_discrete_nonlinear_gaussian_ssm import ContDiscreteNonlinearGaussianSSM
+from continuous_discrete_nonlinear_gaussian_ssm.models import *
+
+from itertools import count
+
+from dynamax.parameters import log_det_jac_constrain
+
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['figure.figsize'] = [16, 9]
+
+
+# %%
+# Simulate synthetic data from true model
+state_dim = 3
+emission_dim = 1
+keys = map(jr.PRNGKey, count())
+
+
+dynamics_drift = {
+    "params": LearnableLorenz63(sigma=10.0, rho=28.0, beta=8/3),
+    "props": LearnableLorenz63(sigma=ParameterProperties(),
+                               rho=ParameterProperties(),
+                               beta=ParameterProperties()) 
+}
+
+dynamics_diffusion_coefficient = {
+    "params": LearnableMatrix(params=jnp.eye(state_dim)),
+    "props": LearnableMatrix(params=ParameterProperties(trainable=False))
+}
+
+dynamics_diffusion_cov = {
+    "params": LearnableMatrix(params=jnp.eye(state_dim)),
+    "props": LearnableMatrix(params=ParameterProperties(constrainer=RealToPSDBijector(), trainable=False))
+}
+
+emission_function = {
+    "params": LearnableLinear(weights=jnp.eye(emission_dim, state_dim), bias=jnp.zeros(emission_dim)),
+    "props": LearnableLinear(weights=ParameterProperties(trainable=False), bias=ParameterProperties(trainable=False))
+}
+
+emission_cov = {
+    "params": LearnableMatrix(params=jnp.eye(emission_dim)),
+    "props": LearnableMatrix(params=ParameterProperties(constrainer=RealToPSDBijector(), trainable=False))
+}
+
+initial_mean = {"params": jnp.zeros(state_dim),
+                "props": ParameterProperties(trainable=False)}
+
+initial_cov = {"params": 100*jnp.eye(state_dim),
+                "props": ParameterProperties(trainable=False, constrainer=RealToPSDBijector())}
+
+
+true_model = ContDiscreteNonlinearGaussianSSM(state_dim, emission_dim)
+true_params, props = true_model.initialize(
+    next(keys),
+    initial_mean=initial_mean,
+    initial_cov=initial_cov,
+    dynamics_drift=dynamics_drift,
+    dynamics_diffusion_coefficient=dynamics_diffusion_coefficient,
+    dynamics_diffusion_cov=dynamics_diffusion_cov,
+    emission_function=emission_function,
+    emission_cov=emission_cov,
+)
+
+# Setup the parser
+parser = argparse.ArgumentParser(description="Run model simulation")
+parser.add_argument('-T', '--total_time', default=10, type=float, help="Total simulation time")
+parser.add_argument('-N', '--num_sequences', default=10, type=int, help="Number of sequences to simulate")
+parser.add_argument('-i', '--num_iterations', default=10, type=int, help="Number of iterations to run")
+parser.add_argument("-b", '--batch_size', default=1, type=int, help="SGD batch size")
+args = parser.parse_args()
+
+num_sequences = args.num_sequences
+T = args.total_time
+num_timesteps = int(T / 0.01)
+t_emissions = jnp.array(sorted(jr.uniform(jr.PRNGKey(0), (num_timesteps, 1), minval=0, maxval=T)))
+# drop duplicates
+t_emissions = jnp.unique(t_emissions)[:, None]
+num_timesteps = len(t_emissions)
+
+true_states, emissions = true_model.sample_batch(
+    true_params, next(keys), num_sequences, num_timesteps, t_emissions, transition_type="path"
+)
+
+def run_sample():
+    ll = true_model.fit_sgd(
+        params=true_params, 
+        props=props,
+        emissions=emissions,
+        filter_hyperparams=EKFHyperParams(), 
+        t_emissions=t_emissions,
+        batch_size=args.batch_size,
+        num_epochs=1,
+    )
+
+import timeit
+# Time the execution
+execution_time = timeit.timeit(run_sample, number=args.num_iterations)
+avg_execution_time = execution_time / args.num_iterations
+print(f"Average execution time: {avg_execution_time} seconds")
---
+++ src/test_scripts/timers/timer_sgd.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define the environments and the parameters
+environments=("hduq_nodynamax" "hduq_nodynamax_GPU")
+sequence_counts=(10 100 1000 10000 100000)
+
+i=1
+T=1
+b=1
+# -i 3: run 3 iterations of the same code block for more robust timing estimates
+# -T 10: This defines the length of the simulated trajectory in model time units
+# -N: The batch number (We simulate N trajectories (each of length T) in parallel)
+# -b: The batch size in SGD
+
+# source the conda.sh script to activate conda
+source ~/anaconda3/etc/profile.d/conda.sh
+
+# Loop over each environment
+for env in "${environments[@]}"; do
+    echo "Activating environment: $env"
+    conda activate "$env"
+    
+    # Loop over each sequence count
+    for N in "${sequence_counts[@]}"; do
+        echo "Running timer.py in environment $env with N=$N"
+        python timer_sgd.py -i "$i" -T "$T" -N "$N" -b "$b"
+    done
+    
+    echo "Deactivating environment: $env"
+    conda deactivate
+done
