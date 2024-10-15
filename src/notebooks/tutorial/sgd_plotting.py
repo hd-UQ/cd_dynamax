9e7be830baee723888cc7e6213a84c0c4330eb76
Iñigo Urteaga <urteaga@gmail.com>
2024-10-15 11:57:58 -0400

    Revised up to HMC

--- src/notebooks/tutorial/sgd_plotting.py
+++ src/notebooks/tutorial/sgd_plotting.py
@@ -4,25 +4,31 @@ from matplotlib import pyplot as plt
 import jax
 
 
-def plot_learning_curve(
-    marginal_lls,
+def plot_mll_learning_curve(
     true_model,
     true_params,
-    # TODO: what are these for?
-    test_model,
-    test_params,
     true_emissions,
     t_emissions,
+    marginal_lls,
 ):
     plt.figure()
-    plt.xlabel("iteration")
+    plt.xlabel("Iterations")
     nsteps = len(marginal_lls)
     true_logjoint = true_model.log_prior(true_params) + true_model.marginal_log_prob(
         true_params, true_emissions, t_emissions
     )
-    plt.axhline(true_logjoint, color="k", linestyle=":", label="true")
-    plt.plot(marginal_lls, label="estimated")
-    plt.ylabel("marginal joint probability")
+    plt.axhline(
+        true_logjoint,
+        color="k",
+        linestyle=":",
+        label="Truth",
+    )
+    plt.plot(
+        marginal_lls,
+        label="Estimated"
+    )
+    plt.ylabel("Marginal log joint probability")
+    plt.title("Marginal log joint probability over iterations")
 
     # Adjust y-axis limits
     y_min = min(min(marginal_lls), true_logjoint) * 1.1  # 10% lower than the smallest value
