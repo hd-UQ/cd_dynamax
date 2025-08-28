ca937644ee58ea017e7aa922ddac1035865bef1a
Matthew Levine <mattlevine22@gmail.com>
2025-08-28 11:47:23 -0400

    modify dynamax typing to enable new requirements

--- dynamax/types.py
+++ dynamax/types.py
@@ -1,9 +1,10 @@
+import jax
 from typing import Optional, Union
 from typing_extensions import Protocol
 from jaxtyping import Array, Float
 import jax._src.random as prng
 
 
-PRNGKey = prng.KeyArray
+PRNGKey = jax.Array   # simple alias; keys are just jax.Array now
 
 Scalar = Union[float, Float[Array, ""]] # python float or scalar jax device array with dtype float
