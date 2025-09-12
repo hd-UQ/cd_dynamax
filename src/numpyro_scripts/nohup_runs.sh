05651eba29eb8a8d78a2b10f74cc3481ce4f6a59
Matthew Levine <mattlevine22@gmail.com>
2025-09-12 12:16:28 -0400

    improving nohup bash script

--- src/numpyro_scripts/nohup_runs.sh
+++ src/numpyro_scripts/nohup_runs.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 PROJECT_NAME="test-nohup-runs-Sep12-2025_0"
 DIR="." # It will put a "wandb/" directory in whatever DIR is set to.
 # If running on your local machine, you can set DIR="." or DIR="$HOME/wandb_runs"
@@ -5,6 +6,7 @@ DIR="." # It will put a "wandb/" directory in whatever DIR is set to.
 # DIR="/data/levinema/cd_dynamax"
 
 # Activate conda env
+eval "$(conda shell.bash hook)" # essentially runs "conda init" in a bash script
 conda activate cd_dynamax-2025
 
 # Take wandb online
@@ -13,39 +15,48 @@ wandb online
 ### ----- L63 DICT (regular timesteps) ------- ###
 #### L = 0.01 (small diffusion) requires covariance inflation to track true state
 # dt=0.001 (small dt)
-nohup python l63_dict_laplace.py --init_lr 0.05 --dt 0.001 --num_epochs 5000 --diffusion_coeff 0.01 --inflation_delta 0.5 --seed 0 --project $PROJECT_NAME --run_name L63-LaplaceDict-L1e-2-dt1e-3 --dir $DIR &
+RUN_NAME="L63-LaplaceDict-L1e-2-dt1e-3"
+nohup python l63_dict_laplace.py --init_lr 0.05 --dt 0.001 --num_epochs 5000 --diffusion_coeff 0.01 --inflation_delta 0.5 --seed 0 --project $PROJECT_NAME --run_name $RUN_NAME --dir $DIR & > $DIR/$RUN_NAME.out 2>&1
 
 # dt=0.01
-nohup python l63_dict_laplace.py --init_lr 0.05 --dt 0.01 --num_epochs 5000 --diffusion_coeff 0.01 --inflation_delta 0.5 --seed 0 --project $PROJECT_NAME --run_name L63-LaplaceDict-L1e-2-dt1e-2 --dir $DIR &
+RUN_NAME="L63-LaplaceDict-L1e-2-dt1e-2"
+nohup python l63_dict_laplace.py --init_lr 0.05 --dt 0.01 --num_epochs 5000 --diffusion_coeff 0.01 --inflation_delta 0.5 --seed 0 --project $PROJECT_NAME --run_name $RUN_NAME --dir $DIR & > $DIR/$RUN_NAME.out 2>&1
 
 # dt=0.1 (large dt)
 # this one needs some optimization tuning! 20000 epochs wasn't really enough. 
-nohup python l63_dict_laplace.py --init_lr 0.005 --dt 0.1 --num_epochs 10000 --diffusion_coeff 0.01 --inflation_delta 0.5 --seed 0 --project $PROJECT_NAME --run_name L63-LaplaceDict-L1e-2-dt1e-1 --dir $DIR &
+RUN_NAME="L63-LaplaceDict-L1e-2-dt1e-1"
+nohup python l63_dict_laplace.py --init_lr 0.005 --dt 0.1 --num_epochs 10000 --diffusion_coeff 0.01 --inflation_delta 0.5 --seed 0 --project $PROJECT_NAME --run_name $RUN_NAME --dir $DIR & > $DIR/$RUN_NAME.out 2>&1
 
 #### L= 1.0 (medium diffusion) ####
 # small dt=0.001 (takes the longest)
-nohup python l63_dict_laplace.py --init_lr 0.1 --dt 0.001 --num_epochs 5000  --diffusion_coeff 1.0 --seed 0 --project $PROJECT_NAME --run_name L63-LaplaceDict-L1-dt1e-3 --dir $DIR &
+RUN_NAME="L63-LaplaceDict-L1-dt1e-3"
+nohup python l63_dict_laplace.py --init_lr 0.1 --dt 0.001 --num_epochs 5000  --diffusion_coeff 1.0 --seed 0 --project $PROJECT_NAME --run_name $RUN_NAME --dir $DIR & > $DIR/$RUN_NAME.out 2>&1
 
 # moderate dt=0.01
-nohup python l63_dict_laplace.py --init_lr 0.1 --dt 0.01 --num_epochs 5000 --diffusion_coeff 1.0 --seed 0 --project $PROJECT_NAME --run_name L63-LaplaceDict-L1-dt1e-2 --dir $DIR &
+RUN_NAME="L63-LaplaceDict-L1-dt1e-2"
+nohup python l63_dict_laplace.py --init_lr 0.1 --dt 0.01 --num_epochs 5000 --diffusion_coeff 1.0 --seed 0 --project $PROJECT_NAME --run_name $RUN_NAME --dir $DIR & > $DIR/$RUN_NAME.out 2>&1
 
 # larger dt=0.1
-nohup python l63_dict_laplace.py --init_lr 0.01 --dt 0.1 --num_epochs 10000  --diffusion_coeff 1.0 --seed 0 --project $PROJECT_NAME --run_name L63-LaplaceDict-L1-dt1e-1 --dir $DIR &
+RUN_NAME="L63-LaplaceDict-L1-dt1e-1"
+nohup python l63_dict_laplace.py --init_lr 0.01 --dt 0.1 --num_epochs 10000  --diffusion_coeff 1.0 --seed 0 --project $PROJECT_NAME --run_name $RUN_NAME --dir $DIR & > $DIR/$RUN_NAME.out 2>&1
 
 #### ------- L63 Neural Net (regular timesteps) ------- ###
-nohup python l63_nn.py --init_lr 0.01 --num_epochs 1000 --dt 0.01 --diffusion_coeff 1.0 --seed 0 --project $PROJECT_NAME --run_name L63-NN-FullObs --dir $DIR &
+RUN_NAME="L63-NN-FullObs"
+nohup python l63_nn.py --init_lr 0.01 --num_epochs 1000 --dt 0.01 --diffusion_coeff 1.0 --seed 0 --project $PROJECT_NAME --run_name $RUN_NAME --dir $DIR & > $DIR/$RUN_NAME.out 2>&1
 
 ##### ------ Partially observed L63 Neural Net (regular timesteps) ------- ###
-nohup python l63_nn.py --num_epochs 5000 --init_lr 0.01 --use_lr_scheduler 1 --emission_dim 1 --project $PROJECT_NAME --run_name L63-NN-PartialObs --dir $DIR &
+RUN_NAME="L63-NN-PartialObs"
+nohup python l63_nn.py --num_epochs 5000 --init_lr 0.01 --use_lr_scheduler 1 --emission_dim 1 --project $PROJECT_NAME --run_name $RUN_NAME --dir $DIR & > $DIR/$RUN_NAME.out 2>&1
 
 #### ------- Van Der Pol GP-expansion (regular timesteps) ------- ###
-nohup python vdp_hsgp_matern.py --num_epochs 10000 --m 10 --init_lr 0.005 --seed 0 --project $PROJECT_NAME --run_name VDP-HSGP-Matern-m10 --dir $DIR &
+RUN_NAME="VDP-HSGP-Matern-m10"
+nohup python vdp_hsgp_matern.py --num_epochs 10000 --m 10 --init_lr 0.005 --seed 0 --project $PROJECT_NAME --run_name $RUN_NAME --dir $DIR & > $DIR/$RUN_NAME.out 2>&1
 
 #### -------- L96 DICT (regular timesteps dt=0.01) ---------- ###
 # 5 states
-nohup python l96_dict_laplace_init_zero.py --init_lr 0.01 --num_epochs 2000 --state_dim 5 --N_particles 25 --project $PROJECT_NAME --run_name L96-LaplaceDict-dx5 --dir $DIR &
+RUN_NAME="L96-LaplaceDict-dx5"
+nohup python l96_dict_laplace_init_zero.py --init_lr 0.01 --num_epochs 2000 --state_dim 5 --N_particles 25 --project $PROJECT_NAME --run_name $RUN_NAME --dir $DIR & > $DIR/$RUN_NAME.out 2>&1
 
 # 10 states
-nohup python l96_dict_laplace_init_zero.py --init_lr 0.01 --num_epochs 1000 --state_dim 10 --N_particles 100 --project $PROJECT_NAME --run_name L96-LaplaceDict-dx10 --dir $DIR &
-
-
+RUN_NAME="L96-LaplaceDict-dx10"
+nohup python l96_dict_laplace_init_zero.py --init_lr 0.01 --num_epochs 1000 --state_dim 10 --N_particles 100 --project $PROJECT_NAME --run_name $RUN_NAME --dir $DIR & > $DIR/$RUN_NAME.out 2>&1
