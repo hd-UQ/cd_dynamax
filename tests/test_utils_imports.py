fff34cecccf5541595a25e89820fd1f897b73ce9
Matthew Levine <mattlevine22@gmail.com>
2025-10-13 12:18:02 -0400

    centralizing cd_dynamax tests

---
+++ tests/test_utils_imports.py
@@ -0,0 +1,5 @@
+def test_utils_imports():
+    """Ensure utils are importable and callable."""
+    from cd_dynamax.src.utils import debug_utils, diffrax_utils
+    assert hasattr(debug_utils, "__file__") or hasattr(debug_utils, "__name__")
+    assert hasattr(diffrax_utils, "diffeqsolve")
