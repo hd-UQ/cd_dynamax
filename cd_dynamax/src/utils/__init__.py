614f7911b811ed9dcd7e878fc0a0600e3aeea4ea
Dan Waxman <dan.waxman1@gmail.com>
2026-03-05 16:33:52 -0500

    Whitespace formatting

--- cd_dynamax/src/utils/__init__.py
+++ cd_dynamax/src/utils/__init__.py
@@ -4,4 +4,3 @@ Utility modules for cd_dynamax's continuous-discrete SSMs.
 This package groups together functions for simulation, optimization,
 evaluation, plotting, and related helpers used throughout the library.
 """
-
